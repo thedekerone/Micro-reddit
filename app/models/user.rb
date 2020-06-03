7d5242e0f6a1a9bc51b98659786dfb53156432f0
Mauricio <mauriciofow@gmail.com>
2020-06-03 16:04:43 -0500

    User created

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :username, presence:true, length:{minimum:4, maximum:12}, uniqueness:true
+    validates :email, presence:true, uniqueness:true
+    validates :password, presence:true, length:{minimum:6,maximum:16}
+end
