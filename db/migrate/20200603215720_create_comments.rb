0ee35bb130297055f6af6b9a176c7f6c1755fda1
Mauricio <mauriciofow@gmail.com>
2020-06-05 10:09:19 -0500

    add validations to comment

--- db/migrate/20200603215720_create_comments.rb
+++ db/migrate/20200603215720_create_comments.rb
@@ -3,7 +3,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
       t.text :body
       t.string :title
-      t.references :commentary, polymorphic:true
+      t.references :user, null:false,foreign_key: true
+      t.references :post, null:false,foreign_key: true 
 
       t.timestamps
     end
